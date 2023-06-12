7cab25181817809bc2554f319f9efb9b24eafa3d
Daniil Nosov <fancy.dogge@gmail.com>
2023-06-12 15:19:23 +0300

    CookieTransport style auth

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from db.models import Base
-from db.database import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from envs import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- main.py
+++ main.py
@@ -1,98 +1,60 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi_users import FastAPIUsers, models
-from fastapi_users.authentication import JWTAuthentication
-from fastapi_users.db import SQLAlchemyUserDatabase
-from datetime import datetime, timedelta
-from pydantic import BaseModel
-from db.database import get_db, engine
-from db.models import User, Salary
-from dotenv import load_dotenv
-import os
-
-
-load_dotenv()
-
-SECRET = os.environ.get('JWT_SECRET')
-ALGORITHM = os.environ.get('JWT_ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('JWT_EXPIRATION_MINUTES'))
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class UserCreate(models.BaseUserCreate):
-    pass
-
-
-class UserDB(models.BaseUserDB):
-    pass
-
-
-class UserUpdate(models.BaseUserUpdate):
-    pass
-
-
-class UserDBModel(UserDB, models.BaseUserDBModel):
-    pass
-
-
-user_db = SQLAlchemyUserDatabase(UserDBModel, engine, User)
-
-
-def get_current_user(
-    user: User = Depends(user_db.get_current_active_user)
-) -> User:
-    return user
-
-
-def get_authentication():
-    return JWTAuthentication(secret=SECRET, lifetime_seconds=ACCESS_TOKEN_EXPIRE_MINUTES * 60)
+from fastapi import FastAPI, Depends
+from db.models import User
+from fastapi_users import fastapi_users, FastAPIUsers
+from api.cfg import auth_backend
+from api.manager import get_user_manager
+from api.schemas import UserCreate, UserRead
 
 
 app = FastAPI()
 
-fastapi_users = FastAPIUsers(
-    user_db,
-    [get_authentication()],
-    User,
-    UserCreate,
-    UserUpdate,
-    UserDBModel,
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-@app.on_event("startup")
-async def startup():
-    await user_db.create()
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
+current_user = fastapi_users.current_user()
 
-@app.on_event("shutdown")
-async def shutdown():
-    await user_db.close()
-    await user_db.drop()
-
-
-@app.post("/login", response_model=Token)
-async def login(
-    credentials: models.UserPasswordCredentials = Depends(fastapi_users.login),
-):
-    user = await user_db.get_by_email(credentials.email)
-    access_token = get_authentication().generate_token(user)
-    token_type = "bearer"
-    return Token(access_token=access_token, token_type=token_type)
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
-@app.get("/salary", response_model=Salary)
-async def get_salary(
-    current_user: User = Depends(get_current_user),
-):
-    salary = await get_db().get(Salary, user_id=current_user.id)
-    if salary:
-        return salary
-    else:
-        raise HTTPException(status_code=404, detail="Salary not found")
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
+
+# @app.get("/salary", response_model=Salary)
+# async def get_salary(
+#     current_user: User = current_user,
+# ):
+#     salary = await get_db().get(Salary, user_id=current_user.id)
+#     if salary:
+#         return salary
+#     else:
+#         raise HTTPException(status_code=404, detail="Salary not found")
 
 
-app.include_router(fastapi_users.router, prefix="/users", tags=["users"])
+# @app.post("/salary", response_model=Salary)
+# async def create_salary(
+#     amount: int,
+#     current_user: User = current_user,
+#     db: Session = Depends(get_db)
+# ):
+#     salary = Salary(amount=amount, user_id=current_user.id)
+#     get_db().add(salary)
+#     get_db().commit()
+#     get_db().refresh(salary)
+#     return salary
